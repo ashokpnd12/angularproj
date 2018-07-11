08b50e730e7155c4607f1d82013e5466fa9709ba
Ashok <Ashok@ASHOK>
2018-07-11 19:40:08 +0530

    ashok

---
+++ ng2prac/ng2prac/src/ComputerArchitecture/Computer.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Computer = /** @class */ (function () {
+    function Computer(_processor) {
+        this._processor = _processor;
+    }
+    return Computer;
+}());
+exports.Computer = Computer;
+var Processor = /** @class */ (function () {
+    function Processor(speed) {
+    }
+    return Processor;
+}());
+exports.Processor = Processor;
+//# sourceMappingURL=Computer.js.map
---
+++ ng2prac/ng2prac/src/ComputerArchitecture/Computer.ts
@@ -0,0 +1,11 @@
+﻿export class Computer {
+    private _processor: Processor
+
+    constructor(_processor: Processor) {
+        this._processor = _processor
+    }
+}
+
+export class Processor {
+    constructor(speed: number) { }
+}
